ab83c7bb4c0c90fe980dbcefaa3e73927fba70c7
DuminduCharuka <99183684+DuminduCharuka@users.noreply.github.com>
2022-04-26 12:09:09 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,16 @@
 #include <stdio.h>
 
 int main() {
-  
+   float avg,mark1,mark2;
+   
+   printf("%s","enter the frist subject marks :");
+   scanf("%f",&mark1);
+   printf("%s","enter the second subject marks :");
+   scanf("%f",&mark2);
+   
+   avg = (mark1+mark2)/2;
+   
+   printf("average is : %.1f",avg);
   return 0;
 }
 
